b8d59da6a56390aa2a8dddcb0fb07ef91bcd7681
IngridGozner <ingrid_gozner@yahoo.com>
2025-07-05 21:49:01 +0300

    add base container

---
+++ src/components/molecules/GridContainer.tsx
@@ -0,0 +1,5 @@
+import { PropsWithChildren } from "react";
+
+export function GridContainer(props: PropsWithChildren) {
+    return <div className="grid grid-cols-6 gap-x-2 md:gap-x-4 lg:grid-cols-12 gap-y-4">{props.children}</div>
+}
