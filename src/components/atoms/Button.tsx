631b4673ad16097afe8a6aab76722ef7447882a2
IngridGozner <ingrid_gozner@yahoo.com>
2025-07-09 18:23:18 +0300

    add button atom

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,22 @@
+import { GoogleIcon, GoogleIconProps } from "./GoogleIcon";
+
+type ButtonProps = Partial<GoogleIconProps> & {
+    className?: string;
+    label?: string;
+    onClick?: () => void;
+    href?: string;
+}
+
+export function Button({ className, label, onClick, href, iconName, iconClassName }: ButtonProps) {
+    const buttonClasses = `py-2 px-3 border-base-dark border-2 flex items-center${className ? ` ${className}` : ''}`;
+
+    const buttonContent = <>{iconName && <GoogleIcon iconName={iconName} iconClassName={iconClassName} />}<span className={iconName && "ml-2"}>{label ? label : ''}</span></>
+
+    return href ?
+        <a href={href} className={buttonClasses}>
+            {buttonContent}
+        </a> :
+        <button className={buttonClasses} onClick={onClick}>
+            {buttonContent}
+        </button>
+}
