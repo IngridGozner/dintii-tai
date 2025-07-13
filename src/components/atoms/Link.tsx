76caacd88574c49f2bfb53edeff13d4f8cfd8d60
IngridGozner <ingrid_gozner@yahoo.com>
2025-07-13 19:10:16 +0300

    add link atom

---
+++ src/components/atoms/Link.tsx
@@ -0,0 +1,20 @@
+import { GoogleIcon, GoogleIconProps } from "./GoogleIcon";
+import NextLink from 'next/link';
+
+type LinkProps = Partial<GoogleIconProps> & {
+    className?: string;
+    label?: string;
+    onClick?: () => void;
+    href: string;
+    darkBackground?: boolean;
+}
+
+export function Link({ className, label, onClick, href, iconName, iconClassName, darkBackground = false }: LinkProps) {
+    const linkClsses = `${darkBackground ? 'text-white hover:text-white' : 'text-link hover:text-link-hover'} flex items-center${className ? ` ${className}` : ''}`;
+
+    const linkContent = <>{iconName && <GoogleIcon iconName={iconName} iconClassName={iconClassName} />}<span className={iconName && "ml-2"}>{label ? label : ''}</span></>
+
+    return <NextLink className={linkClsses} onClick={onClick} href={href}>
+        {linkContent}
+    </NextLink>
+}
