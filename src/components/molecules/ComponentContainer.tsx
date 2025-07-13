af7d5082a11431b0508365ae6eb6d5307d106227
IngridGozner <ingrid_gozner@yahoo.com>
2025-07-13 17:51:45 +0300

    adjust distances for container

--- src/components/molecules/ComponentContainer.tsx
+++ src/components/molecules/ComponentContainer.tsx
@@ -1,8 +1,8 @@
 import { PropsWithChildren } from "react";
 
 export function ComponentContainer({ children, containerClass, contentClass }: PropsWithChildren & { containerClass?: string; contentClass?: string }) {
-    return <div className={`${contentClass ? ' ' + contentClass : ''}`}>
-        <div className={`mx-2 md:mx-4 container${containerClass ? ' ' + containerClass : ''}`}>
+    return <div className={`mt-4 md:mt-8${contentClass ? ' ' + contentClass : ''}`}>
+        <div className={`mx-auto px-5 md:px-10 container${containerClass ? ' ' + containerClass : ''}`}>
             {children}
         </div>
     </div>
