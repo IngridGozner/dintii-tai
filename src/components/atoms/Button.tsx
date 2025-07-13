1ff3df71f67d76646b327009cd3981551fbec87b
IngridGozner <ingrid_gozner@yahoo.com>
2025-07-13 17:51:28 +0300

    add bg to button

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -8,7 +8,7 @@ type ButtonProps = Partial<GoogleIconProps> & {
 }
 
 export function Button({ className, label, onClick, href, iconName, iconClassName }: ButtonProps) {
-    const buttonClasses = `py-2 px-3 border-base-dark border-2 flex items-center${className ? ` ${className}` : ''}`;
+    const buttonClasses = `py-2 px-3 border-base-dark border-2 flex items-center bg-white${className ? ` ${className}` : ''}`;
 
     const buttonContent = <>{iconName && <GoogleIcon iconName={iconName} iconClassName={iconClassName} />}<span className={iconName && "ml-2"}>{label ? label : ''}</span></>
 
