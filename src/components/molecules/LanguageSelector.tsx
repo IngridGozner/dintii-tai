6dd17e9d0ee110ed7bb41d77607c200cad87d8ae
IngridGozner <ingrid_gozner@yahoo.com>
2025-07-13 18:04:22 +0300

    fix animation

--- src/components/molecules/LanguageSelector.tsx
+++ src/components/molecules/LanguageSelector.tsx
@@ -8,14 +8,15 @@ import { LanguageContext } from "../providers/LanguageProvider";
 export function LanguageSelector() {
     const locales = routing.locales;
     const currentLocale = useContext(LanguageContext);
+    const delay = ['delay-75', 'delay-100', 'delay-125']
 
     const [isOpen, setIsOpen] = useState(false);
 
     return <div>
         <Button iconName="public" label={currentLocale?.toUpperCase()} className="cursor-pointer" onClick={() => setIsOpen(!isOpen)} />
-        <div className={`transition-opacity duration-300 ease-in-out ${isOpen ? 'flex flex-col opacity-100' : 'opacity-0'}`}>
-            {locales.map((locale) =>
-                <Button key={locale} label={locale.toUpperCase()} className="flex justify-center mt-2" href={`/${locale}`} />
+        <div className={`transition-opacity duration-300 ease-in-out ${isOpen ? 'flex flex-col opacity-100 visible' : 'opacity-0 invisible'}`}>
+            {locales.map((locale, index) =>
+                <Button key={locale} label={locale.toUpperCase()} className={`flex justify-center mt-2 ${delay[index]}`} href={`/${locale}`} />
             )}
         </div>
     </div>
