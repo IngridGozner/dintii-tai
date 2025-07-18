b29b45edff08ca52da23c98feb3d874755b3d1da
IngridGozner <ingrid_gozner@yahoo.com>
2025-07-18 19:36:56 +0300

    change structure of sanity

--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -1,15 +1,18 @@
-import type {StructureResolver} from 'sanity/structure'
+import type { StructureResolver } from 'sanity/structure'
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Blog')
+    .title('DintiiTai')
     .items([
-      S.documentTypeListItem('post').title('Posts'),
-      S.documentTypeListItem('category').title('Categories'),
-      S.documentTypeListItem('author').title('Authors'),
+      S.documentTypeListItem('siteInfo').title('SiteInfo'),
+      S.documentTypeListItem('treatmentGroup').title('Treatment Groups'),
+      S.documentTypeListItem('treatment').title('Treatments'),
+      S.documentTypeListItem('gallery').title('Gallery'),
+      S.documentTypeListItem('article').title('Posts'),
+
       S.divider(),
       ...S.documentTypeListItems().filter(
-        (item) => item.getId() && !['post', 'category', 'author'].includes(item.getId()!),
+        (item) => item.getId() && !['siteInfo', 'treatmentGroup', 'treatment', 'gallery', 'article'].includes(item.getId()!),
       ),
     ])
