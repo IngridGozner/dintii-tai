1d6938ef7ba5c6049bff3c01382bcae4010e5089
IngridGozner <ingrid_gozner@yahoo.com>
2025-07-18 19:27:48 +0300

    remove unused data

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,15 +1,12 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
 import { blockContentType } from './blockContentType'
-import { categoryType } from './categoryType'
-import { authorType } from './authorType'
 import { siteInfoType } from './siteInfoType'
 import { treatmentType } from './treatmentType'
 import { treatmentGroupType } from './treatmentGroupType'
-import { postType } from './postType'
 import { articleType } from './articleType'
 import { galleryType } from './galleryType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, siteInfoType, categoryType, authorType, treatmentType, treatmentGroupType, postType, articleType, galleryType],
+  types: [blockContentType, siteInfoType, treatmentType, treatmentGroupType, articleType, galleryType],
 }
