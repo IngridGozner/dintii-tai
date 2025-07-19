b4c3823ce2a623215d78bc56fa094311242e0d2a
IngridGozner <ingrid_gozner@yahoo.com>
2025-07-19 13:53:28 +0300

    add burger menu to mobile view

--- src/components/components/Header.tsx
+++ src/components/components/Header.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import NextLink from 'next/link'
 import { GridContainer } from '../molecules/GridContainer';
 import { SITEINFO_QUERYResult } from '@/sanity/types';
@@ -8,10 +10,13 @@ import { Container } from '../molecules/Container';
 import { Button } from '../atoms/Button';
 import { Link } from '../atoms/Link';
 import { useTranslations } from 'next-intl';
+import { useState } from 'react';
+import BurgerMenu from '../molecules/BurgerMenu';
 
 export function Header(props: NonNullable<SITEINFO_QUERYResult>) {
     const t = useTranslations('HomePage');
     const navgationLinks = [{ name: t('rates'), href: '#rates' }, { name: t('aboutUs'), href: '#aboutus' }, { name: 'Contact', href: '#contact' }];
+    const [menuOpen, setMenuOpen] = useState(false);
 
     if (!props) return undefined;
 
@@ -22,12 +27,11 @@ export function Header(props: NonNullable<SITEINFO_QUERYResult>) {
     return (
         <header className="border-b-2 border-base-dark sticky top-0 bg-white shadow-lg z-50">
             <title>{browserTitle}</title>
-
-            <Container contentClass='!py-4 !mt-0'>
+            <Container contentClass='md:!pt-4 !py-0 !mt-0'>
                 <GridContainer>
-                    <div className="col-span-6">
+                    <div className="col-span-5 md:col-span-6">
                         <NextLink
-                            className="flex flex-row mt-7 md:mt-0 text-font"
+                            className="flex flex-row mt-7 mb:mt-2 md:mt-0 text-font justify-start"
                             href="/"
                         >
                             {logo &&
@@ -46,22 +50,29 @@ export function Header(props: NonNullable<SITEINFO_QUERYResult>) {
                             </div>
                         </NextLink>
                     </div>
-                    <div className="col-span-6 flex flex-col items-center justify-center md:items-end">
-                        <div className="flex flex-row top-0 absolute gap-x-4 md:gap-x-8 mt-3 text-base md:text-lg">
-                            {phone && <Button iconName='phone' href={`tel:${phone}`} label={phone} className='h-fit' asLink />}
-                            <LanguageSelector />
-                        </div>
-                        <nav className='md:mt-6'>
-                            <ul className="flex gap-x-8 lg:gap-x-18">
+
+                    <div className="col-span-1 col-start-6 items-end justify-end md:col-span-6 flex md:items-end lg:mb-3">
+                        <BurgerMenu isOpen={menuOpen} toggle={() => setMenuOpen(!menuOpen)} />
+                        <nav className={`absolute top-full left-0 flex justify-center items-center w-full h-96 border-b-2 border-link shadow-2xl bg-background/95 ${menuOpen ? 'block' : 'hidden'} ` +
+                            'md:block md:top-0 md:relative md:h-auto md:bg-transparent md:border-none'}>
+                            <ul className="flex gap-y-8 md:gap-x-8 lg:gap-x-18 flex-col md:flex-row md:justify-end text-2xl md:text-xl">
                                 {navgationLinks.map((link, index) => <li key={link.href + index}>
                                     <Link
-                                        className="!text-font transition-colors text-base lg:text-xl"
+                                        className="!text-font"
                                         href={link.href}
                                         label={link.name}
+                                        onClick={() => setMenuOpen(false)}
                                     />
                                 </li>)}
                             </ul>
                         </nav>
+                    </div>
+
+                    <div className="col-span-6 md:col-span-12 flex items-center justify-center md:items-end md:justify-end">
+                        <div className="flex flex-row top-0 absolute gap-x-4 md:gap-x-8 mt-1 mb:mt-3 text-base md:text-lg">
+                            {phone && <Button iconName='phone' href={`tel:${phone}`} label={phone} className='h-fit' asLink />}
+                            <LanguageSelector />
+                        </div>
                     </div>
                 </GridContainer>
             </Container>
---
+++ src/components/molecules/BurgerMenu.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function BurgerMenu({ isOpen, toggle }: { isOpen: boolean, toggle: () => void }) {
+    const commmonLineClasses = "h-0.5 w-8 bg-link hover:bg-link-hover transition duration-300 ease-in-out";
+
+    return (
+        <button
+            onClick={toggle}
+            className="flex flex-col w-8 h-8 space-y-1.5 md:hidden cursor-pointer"
+            aria-label="Toggle menu"
+        >
+            <span
+                className={`${commmonLineClasses} transform ${isOpen ? 'rotate-45 translate-y-2' : ''
+                    }`}
+            />
+            <span
+                className={`${commmonLineClasses} ${isOpen ? 'opacity-0' : ''
+                    }`}
+            />
+            <span
+                className={`${commmonLineClasses} transform ${isOpen ? '-rotate-45 -translate-y-2' : ''
+                    }`}
+            />
+        </button>
+    );
+}
