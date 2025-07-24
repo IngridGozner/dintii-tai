ff58b9c0ffbf78940734c8e41ed251d77c48a694
IngridGozner <ingrid_gozner@yahoo.com>
2025-07-25 00:41:08 +0300

    add dictionary to structure

--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -10,9 +10,10 @@ export const structure: StructureResolver = (S) =>
       S.documentTypeListItem('treatment').title('Treatments'),
       S.documentTypeListItem('gallery').title('Gallery'),
       S.documentTypeListItem('article').title('Posts'),
+      S.documentTypeListItem('dictionary').title('Dictionary'),
 
       S.divider(),
       ...S.documentTypeListItems().filter(
-        (item) => item.getId() && !['siteInfo', 'treatmentGroup', 'treatment', 'gallery', 'article'].includes(item.getId()!),
+        (item) => item.getId() && !['siteInfo', 'treatmentGroup', 'treatment', 'gallery', 'article', 'dictionary'].includes(item.getId()!),
       ),
     ])
