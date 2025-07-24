ac79e6994485ed44418db3f278144f1a56ed8a82
IngridGozner <ingrid_gozner@yahoo.com>
2025-07-25 01:01:35 +0300

    adjust middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,24 @@
-import createMiddleware from 'next-intl/middleware';
-import { routing } from './i18n/routing';
+import { NextRequest, NextResponse } from "next/server";
 
-export default createMiddleware(routing);
+export const locales = ['en', 'de', 'ro']
+const defaultLocale = 'ro'
+
+export function middleware(request: NextRequest) {
+    // Check if there is any supported locale in the pathname
+    const { pathname } = request.nextUrl
+    const pathnameHasLocale = locales.some(
+        (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+    )
+
+    if (pathnameHasLocale) return
+
+    // Redirect if there is no locale
+    const locale = request.cookies.get('NEXT_LOCALE')?.value || defaultLocale;
+    request.nextUrl.pathname = `/${locale}${pathname}`
+    // e.g. incoming request is /products
+    // The new URL is now /en-US/products
+    return NextResponse.redirect(request.nextUrl)
+}
 
 export const config = {
     // Match all pathnames except for
