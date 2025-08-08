b68b3002f63fa7c3bd0194bffbd3886aa6a4f933
IngridGozner <ingrid_gozner@yahoo.com>
2025-08-08 11:17:13 +0300

    rename patient

--- src/app/[lang]/patients/page.tsx
+++ src/app/[lang]/patients/page.tsx
@@ -1,7 +1,7 @@
 import { createClient } from '@/supabase/server';
 import { redirect } from 'next/navigation'
 
-export default async function Pacients() {
+export default async function Patients() {
     const supabase = await createClient();
 
     //if no user redirect to login
@@ -10,7 +10,7 @@ export default async function Pacients() {
         redirect('/login')
     }
 
-    const { data: pacients } = await supabase.from("pacient").select();
+    const { data: patients } = await supabase.from("patient").select();
 
-    return <pre>{JSON.stringify(pacients, null, 2)}</pre>
+    return <pre>{JSON.stringify(patients, null, 2)}</pre>
 }
