ae362f8bc07037f15ef5e914a10ce3b7cf1f5f6d
IngridGozner <ingrid_gozner@yahoo.com>
2025-08-10 01:21:11 +0300

    add first changes for login page

--- src/app/[lang]/login/page.tsx
+++ src/app/[lang]/login/page.tsx
@@ -1,13 +1,36 @@
+import Dialog from '@/components/components/Dialog'
 import { login } from './actions'
+import { Button } from '@/components/atoms/Button'
+import { Input } from '@/components/atoms/Input'
+import { sanityFetch } from '@/sanity/lib/live';
+import { STAGE_QUERY } from '@/sanity/lib/queries';
+import Image from 'next/image'
+import { urlFor } from '@/sanity/lib/image';
 
-export default function LoginPage() {
+export default async function LoginPage() {
+    const { data: loginData } = await sanityFetch({ query: STAGE_QUERY, params: { language: 'en' } });
+    const backgroundImage = loginData?.stageImage?.image;
+
     return (
-        <form>
-            <label htmlFor="email">Email:</label>
-            <input id="email" name="email" type="email" required />
-            <label htmlFor="password">Password:</label>
-            <input id="password" name="password" type="password" required />
-            <button formAction={login}>Log in</button>
-        </form>
+        <>
+            <Dialog
+                headline='Login'
+            >
+                <form className='flex flex-col gap-y-7'>
+                    <Input label={'Email'} element={'email'} type={'email'} />
+                    <Input label={'Password'} element={'password'} type={'password'} />
+                    <Button formAction={login} label='Login' className='rounded-full text-center justify-center' />
+                </form>
+            </Dialog >
+
+            {backgroundImage &&
+                <Image
+                    src={urlFor(backgroundImage).width(3000).height(1000).url()}
+                    alt={backgroundImage?.alt || ''}
+                    fill
+                    className="object-cover w-full h-full relative"
+                />
+            }
+        </>
     )
 }
--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -6,10 +6,11 @@ type ButtonProps = Partial<GoogleIconProps> & {
     onClick?: () => void;
     href?: string;
     asLink?: boolean;
+    formAction?: (formData: FormData) => Promise<void>
 }
 
-export function Button({ className, label, onClick, href, iconName, iconClassName, asLink }: ButtonProps) {
-    const buttonClasses = `flex items-center ${asLink ? 'text-link hover:text-link-hover' : 'border-base-dark border-2 py-2 px-3 bg-white'} ${className ? ` ${className}` : ''}`;
+export function Button({ className, label, onClick, href, iconName, iconClassName, asLink, formAction }: ButtonProps) {
+    const buttonClasses = `flex items-center cursor-pointer ${asLink ? 'text-link hover:text-link-hover' : 'border-base-dark border-2 py-2 px-3 bg-white'} ${className ? ` ${className}` : ''}`;
 
     const buttonContent = <>{iconName && <GoogleIcon iconName={iconName} iconClassName={iconClassName} />}<span className={iconName && "ml-2"}>{label ? label : ''}</span></>
 
@@ -17,7 +18,7 @@ export function Button({ className, label, onClick, href, iconName, iconClassNam
         <a href={href} className={buttonClasses} onClick={onClick}>
             {buttonContent}
         </a> :
-        <button className={buttonClasses} onClick={onClick}>
+        <button className={buttonClasses} onClick={onClick} formAction={formAction}>
             {buttonContent}
         </button>
 }
