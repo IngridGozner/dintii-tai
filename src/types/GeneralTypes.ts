672174e1de62a608ef1908297294fb173b9ac867
IngridGozner <ingrid_gozner@yahoo.com>
2025-08-10 01:19:54 +0300

    add sanityimage

--- src/types/GeneralTypes.ts
+++ src/types/GeneralTypes.ts
@@ -1,1 +1,20 @@
+import { internalGroqTypeReferenceTo, SanityImageCrop, SanityImageHotspot } from "@/sanity/types";
+
 export type FieldString = { value: string | null };
+
+
+export type SanityImage = {
+    image: {
+        asset?: {
+            _ref: string;
+            _type: "reference";
+            _weak?: boolean;
+            [internalGroqTypeReferenceTo]?: "sanity.imageAsset";
+        };
+        media?: unknown;
+        hotspot?: SanityImageHotspot;
+        crop?: SanityImageCrop;
+        alt?: string;
+        _type: "image";
+    } | null;
+}
