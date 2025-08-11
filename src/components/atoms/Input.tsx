27e71a2a9ca5d91472021ea9b53eb8e82a850b40
IngridGozner <ingrid_gozner@yahoo.com>
2025-08-11 23:40:43 +0300

    minor input styling

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -9,7 +9,7 @@ export function Input(props: InputProps) {
 
     return (
         <div className="relative">
-            <input id={element} type={type} name={element} className="peer w-full border-b placeholder:text-transparent bg-white p-3 rounded-full min-w-72" placeholder="name" />
+            <input id={element} type={type} name={element} className="peer w-full border-b placeholder:text-transparent bg-white p-3 rounded-lg min-w-72" placeholder="name" />
             <label htmlFor={element} className="absolute left-0 ml-3 -translate-y-6 peer-focus:text-white text-white duration-200 ease-linear peer-placeholder-shown:translate-y-2.5 peer-placeholder-shown:text-gray-500 peer-focus:ml-3 peer-focus:-translate-y-6 cursor-text">
                 {label}
             </label>
