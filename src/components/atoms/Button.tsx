4101e1a78b005ba972761ee8b6c95723ea8cfb8c
IngridGozner <ingrid_gozner@yahoo.com>
2025-08-11 23:40:09 +0300

    mino button design

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -10,7 +10,8 @@ type ButtonProps = Partial<GoogleIconProps> & {
 }
 
 export function Button({ className, label, onClick, href, iconName, iconClassName, asLink, formAction }: ButtonProps) {
-    const buttonClasses = `flex items-center cursor-pointer ${asLink ? 'text-link hover:text-link-hover' : 'border-base-dark border-2 py-2 px-3 bg-white'} ${className ? ` ${className}` : ''}`;
+    const buttonClasses = `flex items-center cursor-pointer ${asLink ?
+        'text-link hover:text-link-hover' : 'bg-link border-2 py-2 px-4 text-white hover:bg-link-hover rounded-lg'} ${className ? ` ${className}` : ''}`;
 
     const buttonContent = <>{iconName && <GoogleIcon iconName={iconName} iconClassName={iconClassName} />}<span className={iconName && "ml-2"}>{label ? label : ''}</span></>
 
