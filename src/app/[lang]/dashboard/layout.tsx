352483796b87fdbf42de858b7547f3e4ca882b69
IngridGozner <ingrid_gozner@yahoo.com>
2025-08-12 21:15:02 +0300

    remove unnecessary fields

--- src/app/[lang]/dashboard/layout.tsx
+++ src/app/[lang]/dashboard/layout.tsx
@@ -1,24 +1,15 @@
 import DashboardHeader from '@/components/components/DashboardHeader';
 import Sidebar from '@/components/components/Sidebar';
-import { Providers } from '@/components/providers/providers';
-import { sanityFetch } from '@/sanity/lib/live';
-import { DICTIONARY_QUERY } from '@/sanity/lib/queries';
 import { createClient } from '@/supabase/server';
 import { redirect } from 'next/navigation';
 
 export default async function DashboardLayout({
   children,
-  params,
 }: Readonly<{
   children: React.ReactNode;
   params: Promise<{ lang: string }>;
 }>) {
   const supabase = await createClient();
-  const { lang } = await params;
-  const { data: dictionaryEntries } = await sanityFetch({
-    query: DICTIONARY_QUERY,
-    params: { language: lang },
-  });
 
   //if no user redirect to login
   const { data, error } = await supabase.auth.getUser();
