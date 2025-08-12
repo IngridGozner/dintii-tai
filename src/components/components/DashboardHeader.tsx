9b51744e47c173e673d33a43c8582a0205cdde47
IngridGozner <ingrid_gozner@yahoo.com>
2025-08-12 21:12:42 +0300

    add dashboard header

--- src/components/components/DashboardHeader.tsx
+++ src/components/components/DashboardHeader.tsx
@@ -1,15 +1,33 @@
-import { signOut } from "@/app/[lang]/login/actions";
-import { Button } from "../atoms/Button";
+'use client';
 
-// components/Header.tsx
+import BurgerMenu from '../molecules/BurgerMenu';
+import { useState } from 'react';
+import { Link } from '../atoms/Link';
+import { LanguageSelector } from '../molecules/LanguageSelector';
+
 export default function DashboardHeader() {
-    return (
-        <header className="flex items-center justify-between bg-white p-4 shadow">
-            <h1 className="text-xl font-bold text-gray-800">Clinic Dashboard</h1>
-            <div className="flex items-center space-x-4">
-                <span className="text-font">Hello, Dr. X</span>
-                <Button label="Logout" onClick={signOut} />
-            </div>
-        </header>
-    );
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <header className='bg-base-dark border-background fixed top-0 z-50 w-full border-b-2 shadow'>
+      <div className='px-3 py-3 lg:px-5 lg:pl-3'>
+        <div className='flex items-center justify-between'>
+          <div className='flex items-center justify-start space-x-6 text-white'>
+            <BurgerMenu
+              isOpen={menuOpen}
+              toggle={() => setMenuOpen(!menuOpen)}
+              variant='dark'
+              className='!mt-0 justify-center'
+            />
+            <Link
+              href='/dashboard'
+              label='DintiiTai'
+              className='text-xl text-white'
+            />
+          </div>
+          <LanguageSelector buttonClassNames='!text-white' />
+        </div>
+      </div>
+    </header>
+  );
 }
--- src/components/molecules/LanguageSelector.tsx
+++ src/components/molecules/LanguageSelector.tsx
@@ -1,31 +1,49 @@
 'use client';
 
-import { Button } from "../atoms/Button";
-import { useContext, useState } from "react";
-import { LanguageContext } from "../providers/LanguageProvider";
-import { locales } from "@/middleware";
+import { Button } from '../atoms/Button';
+import { useContext, useState } from 'react';
+import { LanguageContext } from '../providers/LanguageProvider';
+import { locales } from '@/middleware';
+import { usePathname } from 'next/navigation';
 
 type LanguageProps = {
-    className?: string;
-}
+  className?: string;
+  buttonClassNames?: string;
+};
 
-export function LanguageSelector({ className }: LanguageProps) {
-    const currentLocale = useContext(LanguageContext);
+export function LanguageSelector({
+  className,
+  buttonClassNames,
+}: LanguageProps) {
+  const currentLocale = useContext(LanguageContext);
 
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    return <div className={className}>
-        <Button iconName="public" label={currentLocale?.toUpperCase()} className="cursor-pointer" onClick={() => setIsOpen(!isOpen)} asLink />
-        <div className={`transition-opacity duration-300 ease-in-out ${isOpen ? 'flex flex-col opacity-100 visible relative' : 'opacity-0 invisible absolute'}`}>
-            {locales.map((locale) =>
-                <Button
-                    key={locale}
-                    label={locale.toUpperCase()}
-                    className="flex justify-center mt-2 rounded-lg"
-                    href={`/${locale}`}
-                    onClick={() => document.cookie = `NEXT_LOCALE=${locale}; `}
-                />
-            )}
-        </div>
+  //get path without locale
+  const pathname = usePathname().substring(3);
+
+  return (
+    <div className={className}>
+      <Button
+        iconName='public'
+        label={currentLocale?.toUpperCase()}
+        className={`cursor-pointer ${buttonClassNames ? buttonClassNames : ''}`}
+        onClick={() => setIsOpen(!isOpen)}
+        asLink
+      />
+      <div
+        className={`absolute transition-opacity duration-300 ease-in-out ${isOpen ? 'visible flex flex-col opacity-100' : 'invisible opacity-0'}`}
+      >
+        {locales.map((locale) => (
+          <Button
+            key={locale}
+            label={locale.toUpperCase()}
+            className='mt-2 flex justify-center rounded-lg'
+            href={`/${locale}${pathname}`}
+            onClick={() => (document.cookie = `NEXT_LOCALE=${locale}; `)}
+          />
+        ))}
+      </div>
     </div>
+  );
 }
