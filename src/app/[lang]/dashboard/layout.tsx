908dc01b3e68a48414b6f3aabff65a4e40e6e5c8
IngridGozner <ingrid_gozner@yahoo.com>
2025-08-12 21:12:03 +0300

    configure providers

--- src/app/[lang]/dashboard/layout.tsx
+++ src/app/[lang]/dashboard/layout.tsx
@@ -1,25 +1,39 @@
 import DashboardHeader from '@/components/components/DashboardHeader';
 import Sidebar from '@/components/components/Sidebar';
+import { Providers } from '@/components/providers/providers';
+import { sanityFetch } from '@/sanity/lib/live';
+import { DICTIONARY_QUERY } from '@/sanity/lib/queries';
 import { createClient } from '@/supabase/server';
 import { redirect } from 'next/navigation';
 
-export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
-    const supabase = await createClient();
+export default async function DashboardLayout({
+  children,
+  params,
+}: Readonly<{
+  children: React.ReactNode;
+  params: Promise<{ lang: string }>;
+}>) {
+  const supabase = await createClient();
+  const { lang } = await params;
+  const { data: dictionaryEntries } = await sanityFetch({
+    query: DICTIONARY_QUERY,
+    params: { language: lang },
+  });
 
-    //if no user redirect to login
-    const { data, error } = await supabase.auth.getUser()
+  //if no user redirect to login
+  const { data, error } = await supabase.auth.getUser();
 
-    if (error || !data?.user) {
-        redirect('/login')
-    }
+  if (error || !data?.user) {
+    redirect('/login');
+  }
 
-    return (
-        <div className="flex min-h-screen">
-            <Sidebar />
-            <div className="flex-1 flex flex-col">
-                <DashboardHeader />
-                <main className="p-6 bg-gray-100 flex-1">{children}</main>
-            </div>
-        </div>
-    );
+  return (
+    <div className='flex min-h-screen'>
+      <Sidebar />
+      <div className='flex flex-1 flex-col'>
+        <DashboardHeader />
+        <div className='mt-16 p-4 sm:ml-64'>{children}</div>
+      </div>
+    </div>
+  );
 }
--- src/components/providers/providers.tsx
+++ src/components/providers/providers.tsx
@@ -1,16 +1,22 @@
 'use client';
 
 import { PropsWithChildren } from 'react';
+import LanguageProvider from './LanguageProvider';
+import DictionaryProvider from './DictionaryProvider';
+import { DICTIONARY_QUERYResult } from '@/sanity/types';
 
-type ProviderProps = PropsWithChildren;
+type ProviderProps = PropsWithChildren & {
+  language: string;
+  dictionaryEntries: NonNullable<DICTIONARY_QUERYResult> | null;
+};
 
 export function Providers(props: ProviderProps) {
-    const { children } = props;
-    return (
-        <>
-            {/* <DictionaryProvider dictionaryEntries={defaultDictionaryEntries}> */}
-            {children}
-            {/* </DictionaryProvider> */}
-        </>
-    );
+  const { children, language, dictionaryEntries } = props;
+  return (
+    <LanguageProvider language={language}>
+      <DictionaryProvider dictionaryEntries={dictionaryEntries}>
+        {children}
+      </DictionaryProvider>
+    </LanguageProvider>
+  );
 }
