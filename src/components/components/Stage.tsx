fd4f6ee0689291112435252493edb1dd3d642943
IngridGozner <ingrid_gozner@yahoo.com>
2025-09-27 14:13:47 +0300

    add transition to stage

--- src/components/components/Stage.tsx
+++ src/components/components/Stage.tsx
@@ -1,10 +1,18 @@
+'use client';
+
 import { urlFor } from '@/sanity/lib/image';
 import { STAGE_QUERYResult } from '@/sanity/types';
 import Image from 'next/image';
 import { GridContainer } from '../molecules/GridContainer';
+import { useEffect, useState } from 'react';
 
 export default function Stage(props: NonNullable<STAGE_QUERYResult>) {
   const { motto, stageImage, name, profession } = props;
+  const [pageLoaded, setPageLoaded] = useState(false);
+
+  useEffect(() => {
+    setPageLoaded(true);
+  }, []);
 
   if (!stageImage || !stageImage.image) return null;
 
@@ -12,7 +20,9 @@ export default function Stage(props: NonNullable<STAGE_QUERYResult>) {
     <GridContainer>
       <div className='relative col-span-6 h-[250px] w-full md:col-span-12 md:h-[300px] lg:h-[500px]'>
         {motto && (
-          <div className='absolute inset-0 z-10 grid grid-cols-1 items-end md:grid-cols-2 md:items-center'>
+          <div
+            className={`transition-translate absolute inset-0 z-10 grid -translate-x-4/5 grid-cols-1 items-end opacity-0 duration-1000 md:grid-cols-2 md:items-center ${pageLoaded ? 'translate-x-0 opacity-100' : ''}`}
+          >
             <div className='bg-link/50 p-3 lg:p-12'>
               <h1 className='font-[Architects_Daughter] text-3xl text-white text-shadow-lg lg:text-7xl'>
                 {motto.value}
