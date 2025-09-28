5f4accdbea9e459e233024eccd4032f3e533efa3
IngridGozner <ingrid_gozner@yahoo.com>
2025-09-28 22:51:22 +0300

    increase floating icon

--- src/components/components/FloatingIcon.tsx
+++ src/components/components/FloatingIcon.tsx
@@ -5,13 +5,13 @@ import { getWhatsAppLink } from '@/helpers';
 export default function FloatingIcon({ phone }: { phone: string }) {
   return (
     <Link href={getWhatsAppLink(phone)}>
-      <div className='group fixed right-4 bottom-4 z-50 flex aspect-square w-14 items-center justify-center rounded-full bg-black/90 shadow-lg transition-all hover:w-16'>
+      <div className='group fixed right-6 bottom-6 z-50 flex aspect-square w-16 items-center justify-center rounded-full bg-black/90 shadow-lg transition-all hover:w-18'>
         <Image
           src='/whatsapp.svg'
-          height={32}
-          width={32}
+          height={40}
+          width={40}
           alt='whatsapp icon'
-          className='aspect-square transition-all group-hover:w-10'
+          className='aspect-square transition-all group-hover:w-12'
           loading='lazy'
         />
       </div>
